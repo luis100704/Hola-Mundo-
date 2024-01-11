d02f09106fd0b53a7665d1e9b5e38d0972967f82
luis100704 <156086783+luis100704@users.noreply.github.com>
2024-01-11 12:56:59 +0100

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Ilerna 4.2.10
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
